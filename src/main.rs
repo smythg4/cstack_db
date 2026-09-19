1b9954703056e8cb2eeb3e044b63fac6b2ac65a4
Stephen Smith <stephen.w.smith4@icloud.com>
2026-09-19 09:29:33 -0400

    minor cleanup to conform to test harness

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,10 @@ fn print_prompt() {
 }
 
 fn read_input(lines: &mut Lines<BufReader<Stdin>>) -> Result<String, String> {
-    if let Some(line) = lines.next() {
-        match line {
-            Ok(l) => Ok(l),
-            Err(_) => Err(String::from("Error reading input")),
-        }
+    if let Some(Ok(line)) = lines.next() {
+        Ok(line)
     } else {
-        Err(String::from("No input found"))
+        Err(String::from("Error reading input"))
     }
 }
 
@@ -25,14 +22,14 @@ fn main() {
         let line = match read_input(&mut lines) {
             Ok(l) => l,
             Err(e) => {
-                eprintln!("{e}");
+                println!("{e}");
                 std::process::exit(1);
             }
         };
-        if &line == ".exit" {
+        if line == ".exit" {
             std::process::exit(0);
         } else {
-            eprintln!("Unrecognized command '{line}'");
+            println!("Unrecognized command '{line}'");
         }
     }
 }
